fcc31ff5d410b03e3810a6c1dc3a8f24b057b075
NormaBuendia <normabdv@gmail.com>
2024-07-01 10:38:58 -0400

    primer commit

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+# Proveedor de AWS
+provider "aws" {
+    region = "us-west-2"
+}
+
+# Definición de la VPC
+resource "aws_vpc" "nueva_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "nuevaVPC"
+  }
+}
+
+# Definición de una subred pública
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.nueva_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "SubnetPublica"
+  }
+}
+
+# Definición de una subred privada
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.nueva_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  tags = {
+    Name = "SubnetPrivada"
+  }
+}
+
+# Definicion de grupo de seguridad
+resource "aws_security_group" "security_group" {
+  name        = "security_group"
+  description = "Security group for example"
+
+# Reglas de ingreso 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Reglas de salida
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Definición de la instancia EC2
+resource "aws_instance" "instance_nueva" {
+  ami           = "ami-0f226ae5ce4b11922"  
+  instance_type = "t2.micro" 
+  // Especifica el nombre de tu clave SSH 
+  key_name      = "awsnueva"             
+
+  # Asignación del security group creado arriba
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+
+  # Asignación de nombre a la instancia (tag)
+  tags = {
+    Name = "instance_nueva"
+  }	
+
+ user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-get update
+                sudo apt-get install -y nginx
+                echo " Hola Norma " > /var/www/html/index.nginx-debian.html
+                EOF
+
+}
